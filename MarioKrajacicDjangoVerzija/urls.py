4cd857c8e04532462e65e506b62551e94753b994
Mario <krajacic.mario@gmail.com>
2019-05-21 06:58:50 +0200

    Adding login and logout

--- MarioKrajacicDjangoVerzija/urls.py
+++ MarioKrajacicDjangoVerzija/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('', include('landingPage.urls')),
     path('blog/', include('blog.urls')),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
